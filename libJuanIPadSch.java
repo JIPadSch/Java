b5d6c518d90b34d9fad40a04311e12126b42b6c6
JIPadSch <juanipadsch@gmail.com>
2022-03-31 07:32:23 -0300

    Modificaciones y nuevos módulos

--- libJuanIPadSch.java
+++ libJuanIPadSch.java
@@ -9,7 +9,7 @@ public class libJuanIPadSch {
         Scanner scan = new Scanner (System.in);
         int i;
         for (i=0;i<arrInt.length;i++){
-            System.out.println("Llenar pos "+i+" : ");
+            System.out.println("Llenar posición "+i+" : ");
             arrInt[i] = scan.nextInt();
             scan.nextLine();
         }
@@ -26,7 +26,7 @@ public class libJuanIPadSch {
         Scanner scan = new Scanner (System.in);
         int i;
         for (i=0;i<arrChar.length;i++){
-            System.out.println("Llenar pos "+i+" : ");
+            System.out.println("Llenar posición "+i+" : ");
             arrChar[i] = scan.next().charAt(0);
             scan.nextLine();
         }
@@ -43,7 +43,7 @@ public class libJuanIPadSch {
         Scanner scan = new Scanner (System.in);
         int i;
         for (i=0;i<arrString.length;i++){
-            System.out.println("Llenar pos "+i+" : ");
+            System.out.println("Llenar posición "+i+" : ");
             arrString[i] = scan.nextLine();
             scan.nextLine();
         }
@@ -55,7 +55,62 @@ public class libJuanIPadSch {
             System.out.println(" "+arrString[i]+" ");
         }
     }
-    //Modulo que permite verificar si dos arreglos bidimensionales de ints son iguales
+    //Módulo que permite llenar un arreglo de doubles
+    public static void llenarArrDouble (double[] arrDouble){
+        Scanner scan = new Scanner (System.in);
+        int i;
+        for (i=0;i<arrDouble.length;i++){
+            System.out.println("Llenar posición "+i+" : ");
+            arrDouble[i] = scan.nextDouble();
+            scan.nextLine();
+        }
+    }
+    //Módulo que muestra por pantalla un arreglo de doubles
+    public static void imprimirArrDouble (double[] arrDouble){
+        int i;
+        for (i=0;i<arrDouble.length;i++){
+            System.out.println(" "+arrDouble[i]+" ");
+        }
+    }
+    //Módulo que permite llenar un arreglo de booleans
+    public static void llenarArrBoolean (boolean[] arrBoolean){
+        Scanner scan = new Scanner (System.in);
+        int i;
+        for (i=0;i<arrBoolean.length;i++){
+            System.out.println("Llenar posición "+i+" : ");
+            arrBoolean[i] = scan.nextBoolean();
+            scan.nextLine();
+        }
+    }
+    //Módulo que muestra por pantalla un arreglo de booleans
+    public static void imprimirArrBoolean (boolean[] arrBoolean){
+        int i;
+        for (i=0;i<arrBoolean.length;i++){
+            System.out.println(" "+arrBoolean[i]+" ");
+        }
+    }
+    //Módulo que permite llenar un arreglo bidimensional de enteros
+    public static void llenarArrBidInt (int[][] arrBidInt){
+        Scanner scan = new Scanner (System.in);
+        int i,j;
+        for (i=0;i<arrBidInt.length;i++){
+            for(j=0;j<arrBidInt[i].length;j++){
+                System.out.println("Llenar posición "+i+"|"+j+" : ");
+                arrBidInt[i][j] = scan.nextInt();
+                scan.nextLine();
+            }
+        }
+    }
+    //Módulo que muestra por pantalla un arreglo bidimensional de enteros
+    public static void imprimirArrBidInt (int[][] arrBidInt){
+        int i,j;
+        for (i=0;i<arrBidInt.length;i++){
+            for(j=0;j<arrBidInt[i].length;j++){
+                System.out.println(" |"+arrBidInt[i][j]+"| ");
+            }
+        }
+    }
+    //Módulo que permite verificar si dos arreglos bidimensionales de ints son iguales
     public static boolean arrBidIntIguales (int[][] arr1, int[][] arr2){
         int i=0,j;
         boolean esIgual = (arr1.length==arr2.length && arr2[0].length==arr2[0].length);
@@ -72,5 +127,4 @@ public class libJuanIPadSch {
         }
         return esIgual;
     }
-
 }

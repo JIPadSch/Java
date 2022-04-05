ed6104037818f8cc8084b0d4b2ecfd6ace3142aa
JIPadSch <juanipadsch@gmail.com>
2022-04-05 16:41:02 -0300

    Haciendo el TP 2

--- desarrolloDeAlgoritmos/tp2ArreglosBidimensionales/Ejercicio7.java
+++ desarrolloDeAlgoritmos/tp2ArreglosBidimensionales/Ejercicio7.java
@@ -1,8 +1,156 @@
 package desarrolloDeAlgoritmos.tp2ArreglosBidimensionales;
+import java.util.*;
 /**
  *
  * @author JuanPadSch
  */
 public class Ejercicio7 {
-    
+    /*Dada una matriz numérica, se hacen 4 tipos de recorridos
+    1- Por filas de atrás hacia adelante; 2- Por columnas de arriba hacia abajo;
+    3- En espiral; 4- En zig-zag*/
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        char opcion;
+        int[][] matrizCuadrada = new int [3][3];
+        cargarAutoMatrizCuadrada(matrizCuadrada);
+        System.out.println("Trabajaremos con la siguiente matriz:");
+        libJuanIPadSch.imprimirArrBidInt(matrizCuadrada);
+        System.out.println("\n");
+        do{
+            System.out.println("Elija una opción del menú: ");
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("a. Ver el recorrido de la matriz (filas) de atrás hacia adelante");
+            System.out.println("b. Ver el recorrido de la matriz (columnas) de arriba hacia abajo");
+            System.out.println("c. Ver el recorrido de la matriz en espiral");
+            System.out.println("d. Ver el recorrido de la matriz en zig-zag");
+            System.out.println("e. Terminar y salir del programa");
+            System.out.println("------------------------------------------------------------------");
+            opcion=scan.next().charAt(0);
+            scan.nextLine();
+            menu(opcion,matrizCuadrada); 
+        }while (opcion!='e');
+    }
+    /*Menú*/
+    public static void menu(char opcion, int[][] matrizCuadrada){
+        switch (opcion) {
+            case 'a':
+                recorrerAtrasAdelante(matrizCuadrada);  
+                break;
+            case 'b':
+                recorrerArribaAbajo(matrizCuadrada);
+                break;
+            case 'c':
+                break;
+            case 'd':
+                break;
+            case 'e':
+                System.out.println("ADIÓS");
+                break;        
+            default:
+                break;
+        }
+    }
+    /*Módulo que carga automaticamente una matriz cuadrada*/
+    public static void cargarAutoMatrizCuadrada(int[][] matrizCuadrada){
+        int i,j;
+        for (i=0;i<matrizCuadrada.length;i++){
+            for (j=0;j<matrizCuadrada[0].length;i++){
+                matrizCuadrada[i][j]=i+j;
+            }
+        }
+    }
+    /*Módulo que recorre (y muestra) la matriz por filas de atrás hacia adelante*/
+    public static void recorrerAtrasAdelante(int[][] matrizCuadrada){
+        int i,j;
+        for(i=matrizCuadrada.length;i>0;i--){
+            for(j=0;j<matrizCuadrada[0].length;j++){
+                System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+            }
+            System.out.println("\n");
+        }
+    }
+    /*Modulo que recorre (y muestra) la matriz por columnas de arriba hacia abajo*/
+    public static void recorrerArribaAbajo(int[][] matrizCuadrada){
+        int i,j;
+        for (j=0;j<matrizCuadrada[0].length;j++){
+            for (i=0;i<matrizCuadrada.length;i++){
+                System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+            }
+            System.out.println("\n");
+        }
+    }
+    /*Módulo que recorre (y muestra) la matriz en espiral*/
+    public static void recorrerEspiral(int[][] matrizCuadrada){
+        int i=0,j=0,finIda=matrizCuadrada[0].length, finBajada=matrizCuadrada.length, finVuelta=0,finSubida=0,limite=(matrizCuadrada.length+matrizCuadrada[0].length)-1,aux=0;
+        boolean yendo=true,bajando=false,volviendo=false,subiendo=false;
+        while(yendo){
+            System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+            j++;
+            if (j==finIda && aux<limite){
+                bajando=true;
+                finIda--;
+                aux++;
+                System.out.println("\n");
+            }
+            while(bajando){
+                System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+                i++;
+                if (i==finBajada && aux<limite){
+                    bajando=false;
+                    volviendo=true;
+                    finBajada--;
+                    aux++;
+                    System.out.println("\n");
+                }
+                while(volviendo){
+                    System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+                    j--;
+                    if (j==finVuelta && aux<limite){
+                        volviendo=false;
+                        subiendo=true;
+                        finVuelta++;
+                        aux++;
+                        System.out.println("\n");
+                    }
+                    while (subiendo){
+                        System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+                        i--;
+                        if (i==finSubida && aux<limite){
+                            subiendo=false;
+                            finSubida++;
+                            aux++;
+                            System.out.println("\n");
+                        }
+                    }
+                }
+            }
+            if (aux==limite){
+                yendo=false;
+            }
+        }
+    }
+    /*Módulo que recorre (y muestra) la matriz en zig-zag*/
+    public static void recorrerZigZag(int[][] matrizCuadrada){
+        int i=0,j=0;
+        boolean yendo=true, volviendo=false;
+        while(yendo){
+            System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+            j++;
+            if (j==matrizCuadrada.length && i<matrizCuadrada.length){
+                volviendo=true;
+                i++;
+            }
+            while (volviendo){
+                System.out.print(" |"+matrizCuadrada[i][j]+"| ");
+                j--;
+                if(j==0){
+                    volviendo=false;
+                    i++;
+                }
+            }
+            if(i==matrizCuadrada.length){
+                yendo=false;
+            }
+        }
+    }
 }

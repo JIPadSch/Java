906b8071820e192ceaf555711a7c945f8055dd25
jignacio.padron <jignacio.padron@debian-8-amd64.fi.uncoma.edu.ar>
2022-04-06 11:51:58 -0300

    Ej 8

---
+++ desarrolloDeAlgoritmos/tp2ArreglosBidimensionales/Ejercicio8.java
@@ -0,0 +1,58 @@
+package desarrolloDeAlgoritmos.tp2ArreglosBidimensionales;
+import java.util.*;
+public class Ejercicio8 {
+    public static void cargarMatriz(int[][]matriz){
+        //Este modulo permite cargar los elementos de la matriz
+        Scanner sc= new Scanner(System.in);
+        int i=0,j=0;
+        for(i=0;i<matriz.length; i++){
+            for(j=0;j<matriz[0].length; j++){
+                System.out.println("Ingrese el elemento");
+                matriz[i][j] =sc.nextInt();
+            }
+        }
+    }
+    public static boolean esCuadrada(int [][] matriz ){
+    return matriz.length==matriz[0].length;
+    }
+    
+    public static boolean esSimetrica(int [][] matriz ){
+    int  fila, col;
+    boolean simetrica;
+    fila=0;
+    col=0;
+    simetrica=esCuadrada(matriz);
+    while(simetrica && fila<matriz.length){
+    col=fila+1;
+        while(simetrica &&  col<matriz.length){
+            if(matriz[fila][col] != matriz[col][fila]){
+             simetrica=false;
+            }else{
+            col++;
+            }
+        }
+       fila++; 
+      }
+      return simetrica;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc= new Scanner(System.in);
+        int [][] matriz;
+        int longitudFila,longitudColumna;
+        System.out.println("Ingrese el numero de filas de su matriz");
+        longitudFila= sc.nextInt();
+        System.out.println("Ingrese el numero de columnas de su matriz");
+        longitudColumna= sc.nextInt();
+        matriz = new int [longitudFila][longitudColumna];
+        cargarMatriz(matriz);
+        if(esSimetrica(matriz)){
+            System.out.println("Su matriz es simetrica");
+        }else{
+            System.out.println("Su matriz NO es simetrica");
+        }
+    }
+    
+}
+    
+}

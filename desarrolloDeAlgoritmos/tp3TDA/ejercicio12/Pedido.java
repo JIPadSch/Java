319dbb0ed5d46d14d6bffa70d921878fb6e261ea
JuanIPadSch <jignacio.padron@est.fi.uncoma.edu.ar>
2022-04-10 10:59:04 -0300

    Mejorando los comentarios de Pedido

--- desarrolloDeAlgoritmos/tp3TDA/ejercicio12/Pedido.java
+++ desarrolloDeAlgoritmos/tp3TDA/ejercicio12/Pedido.java
@@ -11,6 +11,7 @@ public class Pedido {
     private String direccionCliente;
     private String nombreCliente;
     private int telefonoCliente;
+    /* CONSTRUCTORES */
     /* Método constructor de la clase Pedido */
     public Pedido(int cod, String desc, int[] cost, String dirC, String nombC, int telC){
         codigo=cod;
@@ -20,21 +21,43 @@ public class Pedido {
         nombreCliente=nombC;
         telefonoCliente=telC;
     }
-    /* Setter del atributo codigo */
-    public void setCodigo(int cod){
-        codigo=cod;        
-    }
+    /* OBSERVADORES */
     /* Getter del atributo codigo */
     public int getCodigo(){
         return codigo;
     }
-    /* Setter del atributo descripcion */
-    public void setDescripcion(String desc){
-        descripcion=desc;
-    }
     /* Getter del atributo descripcion */
     public String getDescripcion(){
         return descripcion;
+    }
+    /* Getter del atributo costos */
+    public int[] getCostos(){
+        return costos;
+    }
+    /* Setter del atributo codigo */
+    public void setCodigo(int cod){
+        codigo=cod;        
+    }
+    /* Getter de un elemento especifico del atributo costos */
+    public int getCostoEnI(int pos){
+        return costos[pos];
+    }
+    /* Getter del atributo direccionCliente */
+    public String getDireccionCliente(){
+        return direccionCliente;
+    }
+    /* Getter del atributo nombreCliente */
+    public String getNombreCliente(){
+        return nombreCliente;
+    }
+    /* Getter del atributo telefonoCliente */
+    public int getTelefonoCliente(){
+        return telefonoCliente;
+    }
+    /* MODIFICADORES */
+    /* Setter del atributo descripcion */
+    public void setDescripcion(String desc){
+        descripcion=desc;
     }
     /* Setter del atributo costos */
     public void setCostos(int[] cost){
@@ -44,14 +67,19 @@ public class Pedido {
     public void setCostoEnI(int costEnI, int pos){
         costos[pos]=costEnI;
     }
-    /* Getter del atributo costos */
-    public int[] getCostos(){
-        return costos;
+    /* Setter del atributo direccionCliente */
+    public void setDireccionCliente(String dirC){
+        direccionCliente=dirC;
     }
-    /* Getter de un elemento especifico del atributo costos */
-    public int getCostoEnI(int pos){
-        return costos[pos];
+    /* Setter del atributo nombreCliente */
+    public void setNombreCliente(String nomC){
+        nombreCliente=nomC;
     }
+    /* Setter del atributo telefonoCliente */
+    public void setTelefonoCliente(int telC){
+        telefonoCliente=telC;
+    }
+    /* PROPIOS DEL TIPO */
     /* Método propio del tipo del atributo costos: suma los costos almacenados en el arreglo */
     public int CalcularCosto(){
         int i, calculoCosto=0;
@@ -59,29 +87,5 @@ public class Pedido {
             calculoCosto+=costos[i];
         }
         return calculoCosto;
-    }
-    /* Setter del atributo direccionCliente */
-    public void setDireccionCliente(String dirC){
-        direccionCliente=dirC;
-    }
-    /* Getter del atributo direccionCliente */
-    public String getDireccionCliente(){
-        return direccionCliente;
-    }
-    /* Setter del atributo nombreCliente */
-    public void setNombreCliente(String nomC){
-        nombreCliente=nomC;
-    }
-    /* Getter del atributo nombreCliente */
-    public String getNombreCliente(){
-        return nombreCliente;
-    }
-    /* Setter del atributo telefonoCliente */
-    public void setTelefonoCliente(int telC){
-        telefonoCliente=telC;
-    }
-    /* Getter del atributo telefonoCliente */
-    public int getTelefonoCliente(){
-        return telefonoCliente;
     }
 }
---
+++ desarrolloDeAlgoritmos/tp3TDA/ejercicio2/Mascota.java
@@ -0,0 +1,26 @@
+package desarrolloDeAlgoritmos.tp3TDA.ejercicio2;
+/**
+ *
+ * @author JuanPadSch
+ */
+public class Mascota {
+    /* Ejercicio 2 */
+    private int codigo;
+    private String nombre;
+    private double peso;
+    private int edad;
+    /* CONSTRUCTORES */
+    /* Constructor de mascota con solo código */
+    public Mascota(int cod){
+        codigo=cod;
+    }
+    /* Constructor de mascota */
+    public Mascota(int cod,String nn,double pe,int ed){
+        codigo=cod;
+        nombre=nn;
+        peso=pe;
+        edad=ed;
+    }
+    /* OBSERVADORES */
+    
+}

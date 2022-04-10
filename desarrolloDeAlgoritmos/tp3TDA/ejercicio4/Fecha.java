1d5cbcd8fa12f2b49a630196931b9c9232680d70
JuanIPadSch <jignacio.padron@est.fi.uncoma.edu.ar>
2022-04-10 14:07:16 -0300

    Actualizando

---
+++ desarrolloDeAlgoritmos/tp3TDA/ejercicio4/Fecha.java
@@ -0,0 +1,28 @@
+package desarrolloDeAlgoritmos.tp3TDA.ejercicio4;
+/**
+ *
+ * @author JuanPadSch
+ */
+public class Fecha {
+    /* Ejercicio 4 */
+    /* ATRIBUTOS */
+    private int dia;
+    private int mes;
+    private int año;
+    /* CONSTRUCTORES */
+    /* Constructor de fecha */
+    public Fecha(int d,int m,int a){
+        dia=d;
+        mes=m;
+        año=a;
+    }
+    /* OBSERVADORES */
+
+    /* MODIFICADORES */
+
+    /* PROPIOS DEL TIPO */
+    /* Método diasTranscurridos, dada una fecha, calcula la cantidad de dias transcurridos en el año */
+    /* Método esFechaAnterior, verifica si una fecha es anterior a otra */
+    /* Método calculaFecha, dada un fecha y una cantidad de días, indica cual será la fecha */
+    /* Método esBisiesto, calculo si un año es bisiesto o no */
+}

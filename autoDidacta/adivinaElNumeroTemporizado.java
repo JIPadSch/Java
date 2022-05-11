fa9c87082b37f1e264d0834cd38b32703a9250e3
JuanPadSch <jignacio.padron@est.fi.uncoma.edu.ar>
2022-05-11 16:37:48 -0300

    Actualizando

---
+++ autoDidacta/adivinaElNumeroTemporizado.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+ *
+ * @author JuanPadSch
+ */
+public class adivinaElNumeroTemporizado{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        Random randomNum = new Random();
+        int numAle = randomNum.nextInt(100) + 1, adivinanzaJugador, cantIntentos=0;
+        do{
+            System.out.println("Escriba un numero del 1 al 100: ");
+            adivinanzaJugador = scan.nextInt();
+            scan.nextLine(); //Limpiando el Scanner
+            cantIntentos++; //Contamos la cantidad de veces que intentaron adivinar el numero
+            if (numAle>adivinanzaJugador) System.out.println("El número es mayor! Inténtelo denuevo");
+            if (numAle<adivinanzaJugador) System.out.println("El número es menor! Inténtelo denuevo");
+        }while (adivinanzaJugador != numAle);
+        System.out.println("FELICIDADES! ADIVINASTE CORRECTAMENTE! Tu cantidad de intentos fueron: "+cantIntentos);
+        scan.close();
+    }    
+}

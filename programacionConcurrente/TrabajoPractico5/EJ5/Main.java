92a47977cc3951af34d5140490114f3143394433
JuanIPadSch <jignacio.padron@est.fi.uncoma.edu.ar>
2023-10-18 15:01:03 -0300

    Semaforos generales

--- programacionConcurrente/TrabajoPractico5/EJ5/Main.java
+++ programacionConcurrente/TrabajoPractico5/EJ5/Main.java
@@ -3,7 +3,7 @@ package programacionConcurrente.TrabajoPractico5.EJ5;
 public class Main {
     public static void main(String[] args) {
 
-        Comedor comedor = new Comedor(10);
+        Comedor comedor = new Comedor(100);
 
         Perro perros[] = new Perro[2000];
         Gato gatos[] = new Gato[2000];
